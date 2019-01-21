1e7a979707007c72a8e92b1a5b56e4e9175a40ea
hajunsong <hajunsong90@gmail.com>
2019-01-21 10:51:33 +0900

    20190121 commit

--- Deploy/deploy_cmd.bat
+++ Deploy/deploy_cmd.bat
@@ -1,10 +1,10 @@
 @echo off
-start C:\Qt\5.11.2\msvc2017_64\bin\windeployqt MasterServer.exe
+start D:\Qt\5.12.0\msvc2017_64\bin\windeployqt MasterServer.exe
 echo MasterServer deploy completed.
-start C:\Qt\5.11.2\msvc2017_64\bin\windeployqt MobileRobot.exe
+start D:\Qt\5.12.0\msvc2017_64\bin\windeployqt MobileRobot.exe
 echo MobileRobot deploy completed.
-start C:\Qt\5.11.2\msvc2017_64\bin\windeployqt Manipulator.exe
+start D:\Qt\5.12.0\msvc2017_64\bin\windeployqt Manipulator.exe
 echo Manipulator deploy completed.
-start C:\Qt\5.11.2\msvc2017_64\bin\windeployqt UIMonitor.exe
+start D:\Qt\5.12.0\msvc2017_64\bin\windeployqt UIMonitor.exe
 echo UIMonitor deploy completed.
 #pause
